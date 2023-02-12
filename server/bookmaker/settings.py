1c6f005edaf3aeb164c0dabb618f80482855c909
Szymon Borda <szymon.borda@deployed.pl>
2023-02-12 21:14:09 +0100

    add djangorestframework-simplejwt

--- server/bookmaker/settings.py
+++ server/bookmaker/settings.py
@@ -132,10 +132,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    ],
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
 }
--- server/bookmaker/urls.py
+++ server/bookmaker/urls.py
@@ -17,11 +17,19 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from bookmaker.bets.urls import router as bets_router
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 router = DefaultRouter()
 router.registry.extend(bets_router.registry)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('bookmaker.accounts.urls')),
-    path('', include(router.urls)),
+    path('api/accounts/', include('bookmaker.accounts.urls')),
+    path('api', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
