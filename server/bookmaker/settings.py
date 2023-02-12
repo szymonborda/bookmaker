c5a9234d277cd87ce1c30be99445fd988ce8ebe5
Szymon Borda <szymon.borda@deployed.pl>
2023-02-13 00:22:04 +0100

    add django-cors-headers

--- server/bookmaker/settings.py
+++ server/bookmaker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'bookmaker.accounts',
     'bookmaker.bets',
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'bookmaker.urls'
@@ -141,3 +144,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
